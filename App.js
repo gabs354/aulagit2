50338c24421ad7678ee7df6b621b9898e403d766
unknown <gabrielescorreia@outlook.com>
2022-03-29 10:43:12 -0300

    PlayStore2

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Cabecalho from "./src/components/cabecalho";
 import Botao from "./src/components/Botao";
-
+import Dados from "./Dados/dadosJogos";
+import Jogo from "./src/components/Jogos";
 
 export default function App() {
   return (
@@ -10,8 +11,24 @@ export default function App() {
       <Cabecalho>
 
       </Cabecalho>
+      
       <Botao Logo="ios-game-controller-outline" Texto="Jogos" Cor="#002333" Logo2="book-outline" Texto2="Livros" Cor2="#002333"/>
       
-     </View>
+      <View>
+        <FlatList
+        horizontal={true}
+        data={Dados}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => (
+          <Jogo 
+          titulo={item.nome}
+          imagem={item.imagem}
+          valor={item.valor}
+          />
+        )}
+        />
+      </View>
+    </View>
+
   );
 }
---
+++ Dados/dadosJogos.js
@@ -0,0 +1,36 @@
+const JOGOS = [
+    {
+        id: "01",
+        nome: "Subway Surfers",
+        valor: "R$13.32",
+        imagem: 'subway.jpg'
+    },
+
+    {
+        id: "02",
+        nome: "Yugioh Master Duel",
+        valor: "gratuito",
+        imagem: "yugi.jpg"
+    },
+
+    {
+        id: "03",
+        nome: "Pokemon GO",
+        valor: "gratuito",
+        imagem: "pokemon.jpg"
+    },
+    {
+        id: "04",
+        nome: "Hora de Aventura",
+        valor: "R$9,90",
+        imagem: "aventura.jpg"
+    },
+    {
+        id: "05",
+        nome: "Dragon Ball Fight",
+        valor: "R$18,00",
+        imagem: "dbz.jpg"
+    }
+]
+
+export default JOGOS;
